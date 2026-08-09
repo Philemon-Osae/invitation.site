2c26b28a4100ed3eab3641c9d5f0bcdb64411023
Philemon-Osae <166734184+Philemon-Osae@users.noreply.github.com>
2026-08-09 19:49:38 +0100

    i have changed the countdown codes. and i have added more styles

--- script.js
+++ script.js
@@ -34,25 +34,7 @@ openButton.focus();
 
 }
 
-const weddingDate = new Date("December 12, 2026 10:00:00").getTime();
 
-const timer = setInterval(function() {
-  const now = new Date().getTime();
-  const distance = weddingDate - now;
-
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  document.getElementById("countdown-timer").innerHTML = 
-    days + " days " + hours + " hours " + minutes + " min " + seconds + " sec ";
-
-  if (distance < 0) {
-    clearInterval(timer);
-    document.getElementById("countdown-timer").innerHTML = "It's Wedding Day! 💍";
-  }
-}, 1000);
 
 // Button events
 openButton.addEventListener(
